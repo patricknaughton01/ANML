9f1dfe91cab7fe5dcb948b38035847249838ac53
Patrick Naughton <patricknaughton01@gmail.com>
2021-04-17 18:55:40 -0500

    support for multiple files

--- vis.py
+++ vis.py
@@ -7,21 +7,24 @@ import seaborn as sns
 def main():
 	parser = argparse.ArgumentParser(
 		description="Visualize average accuracy over a run")
-	parser.add_argument("path", type=str,
-		help="path to results (metadata.json) file")
+	parser.add_argument("paths", type=str, nargs="+",
+		help="path to results (metadata.json) file(s)")
 	args = parser.parse_args()
-	with open(args.path, "r") as f:
-		res_json = json.load(f)
-	num_runs = res_json["params"]["runs"]
-	x = res_json["params"]["schedule"]
-	final_results = res_json["results"]["Final Results"]
-	averages = [0] * len(x)
-	for elt in final_results:
-		ind = x.index(elt[0])
-		# Grab the accuracy
-		averages[ind] += elt[1]["0"][0] / num_runs
-	plt.plot(x, averages)
+	for path in args.paths:
+		with open(path, "r") as f:
+			res_json = json.load(f)
+		num_runs = res_json["params"]["runs"]
+		x = res_json["params"]["schedule"]
+		final_results = res_json["results"]["Final Results"]
+		averages = [0] * len(x)
+		name = res_json["params"]["model"].split(".")[0]
+		for elt in final_results:
+			ind = x.index(elt[0])
+			# Grab the accuracy
+			averages[ind] += elt[1]["0"][0] / num_runs
+		plt.plot(x, averages, label=name)
 	plt.ylim([0, 1])
+	plt.legend()
 	plt.xlabel("Tasks")
 	plt.ylabel("Accuracy")
 	plt.grid()
